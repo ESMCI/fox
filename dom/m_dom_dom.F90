5744eac4c533925a13bc5740861d40e343e91989
Jim Edwards <jedwards@ucar.edu>
2020-08-07 10:16:06 -0600

    turn off examples by default

--- dom/m_dom_dom.F90
+++ dom/m_dom_dom.F90
@@ -750,11 +750,11 @@ endif
 
     select case(np%nodeType)
     case (ELEMENT_NODE, ATTRIBUTE_NODE, XPATH_NAMESPACE_NODE)
-      call destroyElementOrAttribute(np, ex)
+      call destroyElementOrAttribute(np,ex)
     case (DOCUMENT_TYPE_NODE)
-      call destroyDocumentType(np, ex)
+      call destroyDocumentType(np,ex)
     case (ENTITY_NODE, NOTATION_NODE)
-      call destroyEntityOrNotation(np, ex)
+      call destroyEntityOrNotation(np,ex)
     case (DOCUMENT_NODE)
       call destroyDocument(np,ex)
     end select
@@ -5847,6 +5847,7 @@ endif
 
 ! Switch off all GC - since this is GC!
     call setGCstate(arg, .false., ex)
+
     if (arg%nodeType/=DOCUMENT_NODE) then
       if (getFoX_checks().or.FoX_INVALID_NODE<200) then
   call throw_exception(FoX_INVALID_NODE, "destroyDocument", ex)
@@ -9717,8 +9718,7 @@ endif
       endif
     endif
 
-! FIXME what if namespace is undeclared? Throw an error *only* if FoX_errors is
-! on, otherwise its taken care of by namespace fixup on serialization
+! FIXME what if namespace is undeclared? Throw an error *only* if FoX_errors is on, otherwise its taken care of by namespace fixup on serialization
 
     quickFix = getGCstate(getOwnerDocument(arg)) &
       .and. arg%inDocument
